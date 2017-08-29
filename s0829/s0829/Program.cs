96e05ccbff479dc62dfd67ab45b44bde58234ab7
AkiOffice73 <akito7273@gmail.com>
2017-08-29 15:38:01 +0800

    s0829 q1
    使專案編譯成功並符合要求的輸出

---
+++ s0829/s0829/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace s0829
+{
+    class Program
+    {
+        /* output: 
+         * 1+
+         * 2+
+         * 3+
+         * 4+
+         * 5+
+         * 6+
+         * 7+
+         * 8+
+         * 9+
+         * 10+
+         * =55
+         */
+        static void Main(string[] args)
+        {
+            int num = 10;
+            int sum = 0;
+            for (int i = 0; i < num; i++);
+            {
+                sum = sum + i
+            Console.WriteLine(i + "+");  }
+
+            Console.WriteLine("=" + sum);
+            Console.ReadLine();
+        }
+    }
+}
